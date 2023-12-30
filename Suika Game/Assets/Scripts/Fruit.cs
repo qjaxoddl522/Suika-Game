4502b6b16d7277d170b2143875579c56710111ab
qjaxoddl522 <50629110+qjaxoddl522@users.noreply.github.com>
2023-12-30 19:58:35 +0900

    기존 프로젝트 연동

---
+++ Suika Game/Assets/Scripts/Fruit.cs
@@ -0,0 +1,213 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameManager manager;
+    public ParticleSystem effect;
+    public int level;
+    public bool isDrag;
+    public bool isMerge; //합쳐지고 있는지 확인
+    public bool isAttach; //최근에 부딫혔는지 확인
+
+    public Rigidbody2D rigid;
+    CircleCollider2D circle;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    float deadTime;
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable() //스크립트가 활성화될때 실행
+    {
+        anim.SetInteger("Level", level); //쌍따옴표는 애니메이션의 파라미터
+    }
+
+    private void OnDisable() //오브젝트 재사용을 위한 비활성화시 초기화
+    {
+        level = 0;
+        isDrag = false;
+        isMerge = false;
+        isAttach = false;
+
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        transform.localScale = Vector3.zero;
+
+        rigid.simulated = false;
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = 0;
+        circle.enabled = true;
+    }
+
+    void Update()
+    {
+        if (isDrag) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //x축 경계 설정
+            float leftBorder = -4.2f + transform.localScale.x / 2f; //테두리 두깨와 과일의 반지름 고려
+            float rightBorder = 4.2f - transform.localScale.x / 2f;
+
+            if (mousePos.x < leftBorder) {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder) {
+                mousePos.x = rightBorder;
+            }
+
+            mousePos.y = 8;
+            mousePos.z = 0;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+        }
+    }
+
+    public void Drag() { //TouchPad->GameManager에서 실행
+        isDrag = true;
+    }
+
+    public void Drop() {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(AttachRoutine());
+    }
+
+    IEnumerator AttachRoutine()
+    {
+        if (isAttach) {
+            yield break;
+        }
+        isAttach = true;
+        manager.SfxPlay(GameManager.sfx.Attach);
+
+        yield return new WaitForSeconds(0.2f);
+
+        isAttach = false;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Fruit") {
+            Fruit other = collision.gameObject.GetComponent<Fruit>();
+
+            if (level == other.level && !isMerge && !other.isMerge && level < 7) {
+                // 자신과 상대 위치 가져오기
+                float meX = transform.position.x;
+                float meY = transform.position.y;
+                float otherX = other.transform.position.x;
+                float otherY = other.transform.position.y;
+
+                //1. 자신이 아래에 있을때
+                //2. 동일한 높이일 때, 자신이 오른쪽에 있을때
+                if (meY < otherY || (meY == otherY && meX > otherX)) {
+                    //상대방은 숨김
+                    other.Hide(transform.position);
+                    //자신 레벨업
+                    LevelUp();
+                }
+            }
+        }
+    }
+
+    public void Hide(Vector3 targetpos)
+    {
+        isMerge = true;
+
+        rigid.simulated = false; //물리효과 비활성화
+        circle.enabled = false; //콜라이더 비활성화
+
+        if(targetpos == Vector3.up * 100) { //게임오버 할 때 터지는 이펙트
+            EffectPlay();
+        }
+
+        StartCoroutine(HideRoutine(targetpos));
+    }
+
+    IEnumerator HideRoutine(Vector3 targetpos)
+    {
+        int frameCount = 0;
+
+        while (frameCount < 20) { //마치 Update처럼 로직 실행
+            frameCount++;
+            if (targetpos != Vector3.up * 100) { //게임오버 아니면
+                transform.position = Vector3.Lerp(transform.position, targetpos, 0.5f);
+            }
+            else if (targetpos == Vector3.up * 100) {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, 0.2f);
+            }
+
+            yield return null; //여러 프레임에 걸쳐 돌도록
+        }
+
+        manager.score += (int)Mathf.Pow(2, level);
+
+        isMerge = false;
+        gameObject.SetActive(false);
+    }
+
+    void LevelUp()
+    {
+        isMerge = true;
+
+        //합쳐지면 속도 0으로 만들기
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0; //회전속도
+
+        StartCoroutine(LevelUpRoutine());
+    }
+
+    IEnumerator LevelUpRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        anim.SetInteger("Level", level + 1); //애니메이터에 성장한 레벨값을 전달
+        EffectPlay();
+        manager.SfxPlay(GameManager.sfx.LevelUp);
+
+        yield return new WaitForSeconds(0.3f); //애니메이션 시간 고려
+        level++;
+
+        manager.maxLevel = Mathf.Max(level, manager.maxLevel); //인자값중에 최대값 반환
+
+        isMerge = false;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish") {
+            deadTime += Time.deltaTime;
+
+            if (deadTime > 2 ) {
+                spriteRenderer.color = new Color(0.9f, 0.2f, 0.2f);
+            }
+            if (deadTime > 5) {
+                manager.GameOver();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish") {
+            deadTime = 0;
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    void EffectPlay()
+    {
+        effect.transform.position = transform.position; //위치지정
+        effect.transform.localScale = transform.localScale; //스케일 맞추기
+        effect.Play();
+    }
+}
---
+++ Suika Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,235 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("------------[ Core ]")]
+    public int score;
+    public int maxLevel;
+    public bool isOver; //게임오버 확인
+
+    [Header("------------[ Object Pooling ]")]
+    public GameObject fruitPrefab;
+    public Transform fruitGroup;
+    public List<Fruit> fruitPool;
+    public GameObject effectPrefab;
+    public Transform effectGroup;
+    public List<ParticleSystem> effectPool;
+    [Range(1, 30)] //아래 변수를 슬라이딩바로
+    public int poolSize;
+    public int poolCursor;
+    public Fruit lastfruit;
+
+    [Header("------------[ Audio ]")]
+    public AudioSource bgmPlayer;
+    public AudioSource[] sfxPlayer;
+    public AudioClip[] sfxClip;
+    public enum sfx { LevelUp, Next, Attach, Button, Over};
+    int sfxCursor;
+
+    [Header("------------[ UI ]")]
+    public GameObject startGroup;
+    public GameObject endGroup;
+    public Text scoreText;
+    public Text maxScoreText;
+    public Text subScoreText;
+
+    [Header("------------[ ETC ]")]
+    public GameObject line;
+    public GameObject bottom;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60; //프레임 고정
+
+        fruitPool = new List<Fruit>();
+        effectPool = new List<ParticleSystem> ();
+        for (int i = 0; i < poolSize; i++) {
+            MakeFruit();
+        }
+
+        if (!PlayerPrefs.HasKey("MaxScore")) { //최고점수가 없으면 0점 저장
+            PlayerPrefs.SetInt("MaxScore", 0);
+        }
+
+        maxScoreText.text = PlayerPrefs.GetInt("MaxScore").ToString();
+    }
+
+    public void GameStart()
+    {
+        //오브젝트 활성화/비활성화
+        line.SetActive(true);
+        bottom.SetActive(true);
+        scoreText.gameObject.SetActive(true);
+        maxScoreText.gameObject.SetActive(true);
+        startGroup.SetActive(false);
+
+        bgmPlayer.Play();
+        SfxPlay(sfx.Button);
+
+        //게임 시작
+        Invoke("NextFruit", 1.5f);
+    }
+
+    Fruit MakeFruit() //과일을 새로 생성하는 함수
+    {
+        //이펙트 생성
+        GameObject instantEffectObj = Instantiate(effectPrefab, effectGroup); //인스턴스 생성함수
+        instantEffectObj.name = "Effect " + effectPool.Count;
+        ParticleSystem instantEffect = instantEffectObj.GetComponent<ParticleSystem>();
+        effectPool.Add(instantEffect); //풀 리스트에 저장
+
+        //과일 생성
+        GameObject instantFruitObj = Instantiate(fruitPrefab, fruitGroup); //인스턴스 생성함수
+        instantFruitObj.name = "Fruit " + fruitPool.Count;
+        Fruit instantFruit = instantFruitObj.GetComponent<Fruit>();
+        instantFruit.manager = this;
+        instantFruit.effect = instantEffect;
+        fruitPool.Add(instantFruit);
+
+        return instantFruit;
+    }
+
+    Fruit GetFruit() //오브젝트 풀에서 과일 가져오는 함수
+    {
+        for(int i = 0; i < fruitPool.Count; i++) {
+            poolCursor = (poolCursor+1) % fruitPool.Count;
+            if (!fruitPool[poolCursor].gameObject.activeSelf) { //비활성화상태과일이 있으면 현재 과일 내보냄
+                return fruitPool[poolCursor];
+            }
+        }
+        return MakeFruit(); //MakeFruit도 Fruit를 반환하므로 함수를 반환해도 문제없다
+    }
+
+    void NextFruit() //다음 과일 지정함수
+    {
+        if (isOver) {
+            return;
+        }
+
+        lastfruit = GetFruit();
+        lastfruit.level = Random.Range(0, maxLevel); //0~7까지
+        lastfruit.gameObject.SetActive(true); //레벨 설정 후 활성화
+
+        SfxPlay(GameManager.sfx.Next);
+        StartCoroutine(WaitNext()); //코루틴 실행
+    }
+
+    IEnumerator WaitNext()
+    {
+        while (lastfruit != null) { 
+            yield return null; //yield를 통해 1프레임 기다림
+        }
+
+        yield return new WaitForSeconds(2.5f);
+
+        NextFruit();
+    }
+
+    public void TouchDown()
+    {
+        if (lastfruit == null) { //시작하기 전 컨트롤되어 에러나는 것 방지
+            return;
+        }
+        lastfruit.Drag();
+    }
+    public void TouchUp()
+    {
+        if (lastfruit == null) {
+            return;
+        }
+        lastfruit.Drop();
+        lastfruit = null; //떨군 과일은 컨트롤 X
+    }
+
+    public void GameOver()
+    {
+        if (isOver) {
+            return;
+        }
+
+        isOver = true;
+
+        StartCoroutine(GameOverRoutine());
+    }
+
+    IEnumerator GameOverRoutine()
+    {
+        //1. 장면 안에 활성화된 모든 과일 가져오기
+        Fruit[] fruits = GameObject.FindObjectsOfType<Fruit>();
+
+        //2. 지우기 전 모든 과일의 물리효과 비활성화
+        for (int i = 0; i < fruits.Length; i++) {
+            fruits[i].rigid.simulated = false;
+        }
+
+        //3. 1번의 목록을 하나씩 접근해서 지우기
+        for (int i = 0; i < fruits.Length; i++) {
+            fruits[i].Hide(Vector3.up * 100); //불가능한 좌표를 넣어 합쳐지는 경우와 분리
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        //최고점수 갱신
+        int maxScore = Mathf.Max(score, PlayerPrefs.GetInt("MaxScore"));
+        PlayerPrefs.SetInt("MaxScore", maxScore);
+
+        //게임오버 UI 표시
+        subScoreText.text = "점수 : " + scoreText.text;
+        endGroup.SetActive(true);
+
+        bgmPlayer.Stop();
+        SfxPlay(sfx.Over);
+    }
+
+    public void Reset()
+    {
+        SfxPlay(sfx.Button);
+        StartCoroutine(ResetCoroutine());
+    }
+
+    IEnumerator ResetCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(0);//0 또는 "Main"
+    }
+
+    public void SfxPlay(sfx type) //효과음 재생
+    {
+        switch (type) {
+            case sfx.LevelUp:
+                sfxPlayer[sfxCursor].clip = sfxClip[Random.Range(0, 3)];
+                break;
+            case sfx.Next:
+                sfxPlayer[sfxCursor].clip = sfxClip[3];
+                break;
+            case sfx.Attach:
+                sfxPlayer[sfxCursor].clip = sfxClip[4];
+                break;
+            case sfx.Button:
+                sfxPlayer[sfxCursor].clip = sfxClip[5];
+                break;
+            case sfx.Over:
+                sfxPlayer[sfxCursor].clip = sfxClip[6];
+                break;
+        }
+        sfxPlayer[sfxCursor].Play();
+        sfxCursor = (sfxCursor + 1) % sfxPlayer.Length; //나머지 연산으로 배열 길이 초과 방지
+    }
+
+    private void Update() //뒤로가기 시 게임 종료
+    {
+        if (Input.GetButtonDown("Cancel")) {
+            Application.Quit();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        scoreText.text = score.ToString();
+    }
+}
